91e1647c93e1c5c2cde8ec077bbdb9bc8d5e997f
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2026-07-24 09:56:51 -0300

    fix(testcontainers): update MySQLContainer import and formatting

--- 1_spring-data-jpa/spring-jpa-books/src/test/java/com/example/books/TestcontainersConfiguration.java
+++ 1_spring-data-jpa/spring-jpa-books/src/test/java/com/example/books/TestcontainersConfiguration.java
@@ -3,16 +3,16 @@ package com.example.books;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.mysql.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration( proxyBeanMethods = false )
 public class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer()
-    {
-        return new MySQLContainer<>( DockerImageName.parse( "mysql:8.1" ) );
-    }
+  @Bean
+  @ServiceConnection
+  MySQLContainer mysqlContainer() {
+    return new MySQLContainer( DockerImageName.parse( "mysql:8.1" ) );
+  }
+
 }
